f92eee5dfc1def81fa982ff65bf1fbb839508257
bazo <bazo737.io@gmail.com>
2021-02-13 05:55:29 +0200

    add basic categorization to reports

--- src/pages/Reports/Create/index.js
+++ src/pages/Reports/Create/index.js
@@ -1,11 +1,55 @@
-import { Grid } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+import { Grid, Text } from '@chakra-ui/react';
 import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+import { observer } from 'mobx-react';
 import ReportCreationWizard from '../../../components/ReportCreationWizard';
 import TransactionsTable from '../../../components/TransactionsTable';
 import ReportStore from '../../../stores/ReportStore';
 
 const reportStore = ReportStore.create({ id: uuid() });
+
+const Chunk = observer(({ chunk, ...props }) =>
+  chunk.chunked === 'Complete' ? (
+    <Grid
+      border="solid 1px green"
+      gridTemplateRows="auto auto"
+      gridColumn="2"
+      {...props}
+    >
+      <Text>
+        {chunk.prop.name} {chunk.identifier.name}
+      </Text>
+      <Text>Sum: {(chunk.sum / 100).toFixed(2)}</Text>
+    </Grid>
+  ) : (
+    <Grid
+      border="solid 1px red"
+      gridTemplateColumns="1fr 1fr"
+      columnGap="10px"
+      gridColumn="1"
+      {...props}
+    >
+      <Grid gridTemplateRows="auto auto">
+        <Text>
+          {chunk.prop.name} {chunk.identifier.name}
+        </Text>
+        <Text>Sum: {(chunk.sum / 100).toFixed(2)}</Text>
+      </Grid>
+
+      {chunk.chunked?.map(c => (
+        <Chunk key={c.prop.name} chunk={c} />
+      ))}
+    </Grid>
+  )
+);
+
+Chunk.propTypes = {
+  chunk: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array
+  ]).isRequired
+};
 
 const Create = () => {
   const [loading, setLoading] = useState(false);
@@ -24,11 +68,18 @@ const Create = () => {
 
   return (
     <Grid
-      gridTemplateColumns="1fr auto"
+      gridTemplateColumns="auto 1fr auto"
       height="100%"
       overflow="hidden"
       columnGap="30px"
     >
+      <Grid height="100%" minWidth="400px" overflow="auto">
+        {reportStore.chunked === 'Complete'
+          ? null
+          : reportStore.chunked?.map(chunk => (
+              <Chunk chunk={chunk} key={chunk.prop.name} />
+            ))}
+      </Grid>
       <TransactionsTable
         store={reportStore.transactions}
         loading={loading}
@@ -44,4 +95,4 @@ const Create = () => {
   );
 };
 
-export default Create;
+export default observer(Create);
--- src/stores/ReportStore/index.js
+++ src/stores/ReportStore/index.js
@@ -1,23 +1,109 @@
+/* eslint-disable no-param-reassign */
 import { getSnapshot, types } from 'mobx-state-tree';
 import AccountListStore from '../../models/AccountList.store';
 import TransactionListStore from '../../models/TransactionList.store';
 import CalendarStore from '../CalendarStore';
+import sum from '../../helpers/sum';
 
-const ReportStore = types.model('Report', {
-  id: types.identifier,
-  date: types.optional(
-    CalendarStore,
-    getSnapshot(CalendarStore.create({}))
-  ),
-  accounts: types.optional(
-    AccountListStore,
-    getSnapshot(AccountListStore.create({}))
-  ),
-  transactions: types.optional(
-    TransactionListStore,
-    getSnapshot(TransactionListStore.create({}))
-  ),
-  crated_at: types.maybeNull(types.Date)
-});
+const chunk = (transactions, prop) => {
+  if (!transactions?.length || !prop) {
+    // eslint-disable-next-line no-console
+    console.log('Error');
+    return [];
+  }
+  return transactions.reduce((prev, t) => {
+    const targetProp = prop.itemIdentifier(t);
+    if (!targetProp) return prev;
+    const targetChunk = prev.find(c =>
+      prop.validator(c, t)
+    );
+    if (!targetChunk?.transactions?.length) {
+      prev.push({
+        identifier: prop.chunkIdentifier(t),
+        prop,
+        transactions: [t]
+      });
+      return prev;
+    }
+    targetChunk.transactions.push(t);
+    return prev;
+  }, []);
+};
+
+const multiChunk = (transactions, props) => {
+  if (!props.length || !transactions?.length) {
+    return 'Complete';
+  }
+  return chunk(transactions, props[0]).map(c => {
+    const chunked = multiChunk(
+      c.transactions,
+      props.slice(1)
+    );
+    return {
+      ...c,
+      chunked,
+      paid: sum(c.transactions.filter(t => t.amount < 0)),
+      recieved: sum(
+        c.transactions.filter(t => t.amount > 0)
+      ),
+      sum: sum(c.transactions)
+    };
+  });
+};
+
+const ReportStore = types
+  .model('Report', {
+    id: types.identifier,
+    date: types.optional(
+      CalendarStore,
+      getSnapshot(CalendarStore.create({}))
+    ),
+    accounts: types.optional(
+      AccountListStore,
+      getSnapshot(AccountListStore.create({}))
+    ),
+    transactions: types.optional(
+      TransactionListStore,
+      getSnapshot(TransactionListStore.create({}))
+    ),
+    crated_at: types.maybeNull(types.Date)
+  })
+  .views(self => ({
+    get chunked() {
+      const props = [
+        {
+          name: 'account',
+          itemIdentifier: item => item.account?.id,
+          chunkIdentifier: item => item.account,
+          validator: (c, item) =>
+            c.identifier.id === item.account?.id
+        },
+        {
+          name: 'category',
+          itemIdentifier: item => item.category?.id,
+          chunkIdentifier: item => item.category,
+          validator: (c, item) =>
+            c.identifier.id === item.category?.id
+        },
+        {
+          name: 'payee',
+          itemIdentifier: item => item.payee?.id,
+          chunkIdentifier: item => item.payee,
+          validator: (c, item) =>
+            c.identifier.id === item.payee?.id
+        }
+      ];
+      return multiChunk(
+        self.transactions.sortedItems.map(t => ({
+          ...t,
+          category: t.category || { id: 'transfer' },
+          payee: t.payee.name
+            ? t.payee
+            : { ...t.payee, name: t.payee.account.name }
+        })),
+        props
+      );
+    }
+  }));
 
 export default ReportStore;
